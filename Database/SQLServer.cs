02d3fe36f972c8302c1d46b1728f1817b6ce6d5c
almoshammer <dkoqhk13013@gmail.com>
2024-01-12 23:07:19 +0300

    add default value for sql float types

--- Database/SQLServer.cs
+++ Database/SQLServer.cs
@@ -203,9 +203,9 @@ namespace CoffeDX.Database
             if (tp == typeof(int)) return "Int";
             if (tp == typeof(DateTime)) return "Date";
             if (tp == typeof(DateTimeOffset)) return "DateTimeOffset";
-            if (tp == typeof(double)) return "Float";
-            if (tp == typeof(float)) return "Float";
-            if (tp == typeof(decimal)) return "Float";
+            if (tp == typeof(double)) return "Float default 0";
+            if (tp == typeof(float)) return "Float default 0";
+            if (tp == typeof(decimal)) return "Float default 0";
 
             if (tp == typeof(long?)) return "BIGINT NULL";
             if (tp == typeof(byte?[])) return "Image NULL";
@@ -214,9 +214,9 @@ namespace CoffeDX.Database
             if (tp == typeof(int?)) return "Int NULL";
             if (tp == typeof(DateTime?)) return "Date NULL";
             if (tp == typeof(DateTimeOffset?)) return "DateTimeOffset NULL";
-            if (tp == typeof(double?)) return "Float NULL";
-            if (tp == typeof(float?)) return "Float NULL";
-            if (tp == typeof(decimal?)) return "Float NULL";
+            if (tp == typeof(double?)) return "Float NULL default 0";
+            if (tp == typeof(float?)) return "Float NULL default 0";
+            if (tp == typeof(decimal?)) return "Float NULL default 0";
 
             return "Varchar(20)";
         }
