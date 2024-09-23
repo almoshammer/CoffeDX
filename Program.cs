d8a7315039fa2a6bbf2b2763f614d9b977874c2d
almoshammer <dkoqhk13013@gmail.com>
2024-09-23 16:19:58 +0300

    .

--- Program.cs
+++ Program.cs
@@ -1,27 +1,4 @@
-﻿using CoffeDX.Database;
-using CoffeDX.Test;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace CoffeDX
+﻿namespace CoffeDX
 {
     public enum ON_CONSTRAINT_EVENT { NOACTION, CASCADE };
-    //public static class Program
-    //{
-    //    public static void Main()
-    //    {
-    //        SQLServer.DBName = "db_newmax001";
-    //        SQLServer.AUTH_TYPE = AUTHTYPE.LOCAL;
-    //        SQLServer.ServerName = ".\\SQLSERVER_BELAL";
-
-    //        var assm = Assembly.GetAssembly(typeof(BaseModel));
-    //        SQLServer.Migrate(assm);
-    //        Console.Write("Completed");
-    //    }
-    //}
 }
